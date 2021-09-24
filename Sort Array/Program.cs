3266e849a7bd62f3844731b31d3f984caa5bbbdb
Davide Rossi (3I) <DAVIDE.ROSSI.3i@local.ittcesena.it>
2021-09-24 08:31:31 +0200

    Prima versione

---
+++ Sort Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10] { 0,100,20,30,4,65,60,63,80,90};
+
+
+
+            Array.Sort(array);
+        }
+    }
+}
